94e7d7341afa046c2522e1c8bc5ade644356e084
chenshi <chenshi@CHENS-PC>
2017-12-06 16:49:51 +0800

    test

--- quanhu-spring1/src/test/java/org/quanhu/spring1/Day1Test.java
+++ quanhu-spring1/src/test/java/org/quanhu/spring1/Day1Test.java
@@ -1,6 +1,12 @@
 /**Created	by	chenshi  at	2017年12月5日 上午11:50:12*/
 package org.quanhu.spring1;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.quanhu.spring1.day1.entity.User;
@@ -18,15 +24,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Day1Test {
 	
 	private	ApplicationContext	act=null;
+	private	InputStream inputStream=null;
+	private	Properties properties = new	Properties();
 	
 	@Before
 	public	void	createApplicationContext(){
 		act=new		ClassPathXmlApplicationContext("applicationContext/applicationContext.xml");
+		try {
+			inputStream = getClass().getResourceAsStream("/config/jdbc.properties");
+			properties.load(inputStream);
+		} catch (IOException e) {
+			System.out.println("捕获带了io异常");
+		}
 	}
 	
 	@Test
-	public	void	createObject(){
+	public	void	createUser(){
 		User user = (User) act.getBean("user");
+		System.out.println(user.toString());
+	}
+	
+	@Test
+	public	void	createUser2(){
+		User user = (User) act.getBean("user2");
 		System.out.println(user.toString());
 	}
 	
@@ -38,6 +58,13 @@ public class Day1Test {
 		userService.regist("chenshi", "1234565");
 	}
 	
+	@Test
+	public	void	properties(){
+		Set<Entry<Object, Object>> entrySet = properties.entrySet();
+		for (Entry<Object, Object> entry : entrySet) {
+			System.out.println(entry.getKey()+"="+entry.getValue());
+		}
+	}
 	
 
 }
